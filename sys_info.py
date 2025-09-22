c76cdb07eb8291c10334744c9f74137210e11f65
stephenjamesada <ada_stephen@proton.me>
2025-09-22 16:46:16 -0500

    Create existing repository

---
+++ sys_info.py
@@ -0,0 +1,183 @@
+from tabulate import tabulate
+import tabulate_data
+import argparse
+import pyfiglet
+
+figlet = pyfiglet.Figlet()
+
+def clear():
+    print("\033c", end="")
+
+system_information = tabulate(tabulate_data.gen_sys, headers=["key", "value"], tablefmt="simple_outline")
+cpu_stat = tabulate(tabulate_data.cpu_statistics, headers=["key", "value"], tablefmt="simple_outline")
+cpu_time = tabulate(tabulate_data.cpu_times, headers=["key", "value"], tablefmt="simple_outline")
+cpu_counts = tabulate(tabulate_data.count_cpu, headers=["key", "value"], tablefmt="simple_outline")
+cpu_freqs = tabulate(tabulate_data.cpu_frequencies, headers=["key", "value"], tablefmt="simple_outline")
+cpu_aver = tabulate(tabulate_data.cpu_average, headers=["key", "value"], tablefmt="simple_outline")
+virt_mem = tabulate(tabulate_data.vir_mem, headers=["key", "value"], tablefmt="simple_outline")
+swap_mem = tabulate(tabulate_data.swap_mem, headers=["key", "value"], tablefmt="simple_outline")
+dsk_usg = tabulate(tabulate_data.dsk_usg, headers=["key", "value"], tablefmt="simple_outline")
+
+parser = argparse.ArgumentParser(description="Pythonic resource tables.")
+
+parser.add_argument("--all",
+                    action="store_true",
+                    help="Outputs all parsable system information"
+)
+
+parser.add_argument("--general-info",
+                     action="store_true",
+                     help="Outputs general information about the system")
+
+parser.add_argument("--cpu-stats",
+                     action="store_true",
+                     help="Outputs CPU statistics")
+
+parser.add_argument("--cpu-time",
+                     action="store_true",
+                     help="Outputs CPU times")
+
+parser.add_argument("--cpu-count",
+                     action="store_true",
+                     help="Outputs total CPU count"
+)
+
+parser.add_argument("--cpu-frequency",
+                    action="store_true",
+                    help="Outputs current, minimum, and maximum CPU frequencies"
+)
+
+parser.add_argument("--cpu-average",
+                    action="store_true",
+                    help="Outputs CPU average")
+
+parser.add_argument("--cpu-all",
+                    action="store_true",
+                    help="Outputs all CPU-related information"
+)
+
+parser.add_argument("--virtual-mem",
+                    action="store_true",
+                    help="Outputs virtual memory statistics")
+
+parser.add_argument("--swap-mem",
+                    action="store_true",
+                    help="Outputs swap memory statistics")
+
+parser.add_argument("--disk-usage",
+                    action="store_true",
+                    help="Outputs current disk usage on the system")
+
+args = parser.parse_args()
+
+if args.general_info:
+    clear()
+    print(figlet.renderText("System Info"))
+    print("━" * 56)
+    print(f"\n{system_information}\n")
+elif args.cpu_stats:
+    clear()
+    print(figlet.renderText("CPU Stats"))
+    print("━" * 43)
+    print(f"\n{cpu_stat}\n")
+elif args.cpu_time:
+    clear()
+    print(figlet.renderText("CPU Times"))
+    print("━" * 48)
+    print(f"\n{cpu_time}\n")
+elif args.cpu_count:
+    clear()
+    print(figlet.renderText("CPU Count"))
+    print("━" * 48)
+    print(f"\n{cpu_counts}\n")
+elif args.cpu_frequency:
+    clear()
+    print(figlet.renderText("CPU Frequencies"))
+    print("━" * 75)
+    print(f"\n{cpu_freqs}\n")
+elif args.cpu_average:
+    clear()
+    print(figlet.renderText("CPU Average"))
+    print("━" * 57)
+    print(f"\n{cpu_aver}\n")
+elif args.cpu_all:
+    clear()
+    print(figlet.renderText("All CPU Info"))
+    print(f"\n{cpu_stat}\n{cpu_time}\n{cpu_counts}\n{cpu_freqs}\n{cpu_aver}")
+elif args.virtual_mem:
+    clear()
+    print(figlet.renderText("Virtual Memory"))
+    print("━" * 75)
+    print(f"\n{virt_mem}\n")
+elif args.swap_mem:
+    clear()
+    print(figlet.renderText("Swap Memory"))
+    print("━" * 66)
+    print(f"\n{swap_mem}\n")
+elif args.disk_usage:
+    clear()
+    print(figlet.renderText("Disk Usage"))
+    print("━" * 47)
+    print(f"\n{dsk_usg}\n")
+elif args.all:
+    clear()
+    print(figlet.renderText("System Info"))
+    print("━" * 56)
+    print(f"\n{system_information}\n")
+    print(figlet.renderText("CPU Stats"))
+    print("━" * 43)
+    print(f"\n{cpu_stat}\n")
+    print(figlet.renderText("CPU Times"))
+    print("━" * 48)
+    print(f"\n{cpu_time}\n")
+    print(figlet.renderText("CPU Count"))
+    print("━" * 48)
+    print(f"\n{cpu_counts}\n")
+    print(figlet.renderText("CPU Frequencies"))
+    print("━" * 75)
+    print(f"\n{cpu_freqs}\n")
+    print(figlet.renderText("CPU Average"))
+    print("━" * 57)
+    print(f"\n{cpu_aver}\n")
+    print(figlet.renderText("Virtual Memory"))
+    print("━" * 75)
+    print(f"\n{virt_mem}\n")
+    print(figlet.renderText("Swap Memory"))
+    print("━" * 66)
+    print(f"\n{swap_mem}\n")
+    print(figlet.renderText("Disk Usage"))
+    print("━" * 47)
+    print(f"\n{dsk_usg}\n")
+else:
+    clear()
+    print(figlet.renderText("System Info"))
+    print("━" * 56)
+    print(f"\n{system_information}\n")
+    print(figlet.renderText("CPU Stats"))
+    print("━" * 43)
+    print(f"\n{cpu_stat}\n")
+    print(figlet.renderText("CPU Times"))
+    print("━" * 48)
+    print(f"\n{cpu_time}\n")
+    print(figlet.renderText("CPU Count"))
+    print("━" * 48)
+    print(f"\n{cpu_counts}\n")
+    print(figlet.renderText("CPU Frequencies"))
+    print("━" * 75)
+    print(f"\n{cpu_freqs}\n")
+    print(figlet.renderText("CPU Average"))
+    print("━" * 57)
+    print(f"\n{cpu_aver}\n")
+    print(figlet.renderText("Virtual Memory"))
+    print("━" * 75)
+    print(f"\n{virt_mem}\n")
+    print(figlet.renderText("Swap Memory"))
+    print("━" * 66)
+    print(f"\n{swap_mem}\n")
+    print(figlet.renderText("Disk Usage"))
+    print("━" * 47)
+    print(f"\n{dsk_usg}\n")
+        
+"""
+I recommend you use a pager when you run this flagless, or when you run the "--all" flag.
+"""
---
+++ tabulate_data.py
@@ -0,0 +1,92 @@
+import platform
+import psutil
+from psutil._common import bytes2human
+
+cpu_times = psutil.cpu_times()
+cpu_stats = psutil.cpu_stats()
+cpu_freq = psutil.cpu_freq()
+load_average = psutil.getloadavg()
+virtual_memory = psutil.virtual_memory()
+swap_memory = psutil.swap_memory()
+disk_usage = psutil.disk_usage('/')
+
+gen_sys = [
+    ["system", platform.system()],
+    ["machine", platform.machine()],
+    ["node", platform.node()],
+    ["platform", platform.platform()],
+    ["processor", platform.processor()],
+    ["release", platform.release()],
+]
+
+# CPU
+
+count_cpu = [
+    ["cpu count", psutil.cpu_count()]
+]
+
+cpu_times = [
+    ["user", bytes2human(cpu_times.user)],
+    ["nice", bytes2human(cpu_times.nice)],
+    ["system", bytes2human(cpu_times.system)],
+    ["idle", bytes2human(cpu_times.idle)],
+    ["iowait", bytes2human(cpu_times.iowait)],
+    ["interrupt request", bytes2human(cpu_times.irq)],
+    ["soft interrupt request", bytes2human(cpu_times.softirq)],
+    ["steal", bytes2human(cpu_times.steal)],
+    ["guest", bytes2human(cpu_times.guest)],
+    ["guest nice", bytes2human(cpu_times.guest_nice)]
+]
+
+cpu_statistics = [
+    ["ctx switches", cpu_stats.ctx_switches],
+    ["interrupts", cpu_stats.interrupts],
+    ["soft interrupts", cpu_stats.soft_interrupts],
+    ["syscalls", cpu_stats.syscalls]
+]
+
+cpu_frequencies = [
+    ["current", cpu_freq.current],
+    ["min", cpu_freq.min],
+    ["max", cpu_freq.max]
+]
+
+cpu_average = [
+    ["load average 1", load_average[0]],
+    ["load average 2", load_average[1]],
+    ["load average 3", load_average[2]]
+]
+
+# Memory
+
+vir_mem = [
+    ["total", bytes2human(virtual_memory.total)],
+    ["available", bytes2human(virtual_memory.available)],
+    ["percent", bytes2human(virtual_memory.percent)],
+    ["used", bytes2human(virtual_memory.used)],
+    ["free", bytes2human(virtual_memory.free)],
+    ["active", bytes2human(virtual_memory.active)],
+    ["inactive", bytes2human(virtual_memory.inactive)],
+    ["buffers", bytes2human(virtual_memory.buffers)],
+    ["cached", bytes2human(virtual_memory.cached)],
+    ["shared", bytes2human(virtual_memory.shared)],
+    ["slab", bytes2human(virtual_memory.slab)]
+]
+
+swap_mem = [
+    ["total", bytes2human(swap_memory.total)],
+    ["used", bytes2human(swap_memory.used)],
+    ["free", bytes2human(swap_memory.free)],
+    ["percent", f"{swap_memory.free:.2%}"],
+    ["swap-in", bytes2human(swap_memory.sin)],
+    ["swap-out", bytes2human(swap_memory.sout)]
+]
+
+# Disk
+
+dsk_usg = [
+    ["total", bytes2human(disk_usage.total)],
+    ["used", bytes2human(disk_usage.used)],
+    ["free", bytes2human(disk_usage.free)],
+    ["percent", f"{disk_usage.percent:.2%}"]
+]
