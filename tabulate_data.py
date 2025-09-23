a1681a829816d92fa32946f64a0f618ef77e6f10
stephenjamesada <ada_stephen@proton.me>
2025-09-23 15:44:57 -0500

    Add cyan color to tabulate_data.py values with colorama

--- tabulate_data.py
+++ tabulate_data.py
@@ -5,6 +5,7 @@ import colorama
 from colorama import Fore, Back, Style
 
 green = Fore.GREEN
+cyan = Fore.CYAN
 reset = Style.RESET_ALL
 
 cpu_times = psutil.cpu_times()
@@ -16,82 +17,82 @@ swap_memory = psutil.swap_memory()
 disk_usage = psutil.disk_usage('/')
 
 gen_sys = [
-    [f"{green}system{reset}", f"{green}{platform.system()}{reset}"],
-    [f"{green}machine{reset}", f"{green}{platform.machine()}{reset}"],
-    [f"{green}node{reset}", f"{green}{platform.node()}{reset}"],
-    [f"{green}platform{reset}", f"{green}{platform.platform()}{reset}"],
-    [f"{green}processor{reset}", f"{green}{platform.processor()}{reset}"],
-    [f"{green}release{reset}", f"{green}{platform.release()}{reset}"],
+    [f"{green}system{reset}", f"{cyan}{platform.system()}{reset}"],
+    [f"{green}machine{reset}", f"{cyan}{platform.machine()}{reset}"],
+    [f"{green}node{reset}", f"{cyan}{platform.node()}{reset}"],
+    [f"{green}platform{reset}", f"{cyan}{platform.platform()}{reset}"],
+    [f"{green}processor{reset}", f"{cyan}{platform.processor()}{reset}"],
+    [f"{green}release{reset}", f"{cyan}{platform.release()}{reset}"],
 ]
 
 # CPU
 
 count_cpu = [
-    [f"{green}cpu count{reset}", psutil.cpu_count()]
+    [f"{green}cpu count{reset}", f"{cyan}{psutil.cpu_count()}{reset}"]
 ]
 
 cpu_times = [
-    [f"{green}user{reset}", bytes2human(cpu_times.user)],
-    [f"{green}nice{reset}", bytes2human(cpu_times.nice)],
-    [f"{green}system{reset}", bytes2human(cpu_times.system)],
-    [f"{green}idle{reset}", bytes2human(cpu_times.idle)],
-    [f"{green}iowait{reset}", bytes2human(cpu_times.iowait)],
-    [f"{green}interrupt request{reset}", bytes2human(cpu_times.irq)],
-    [f"{green}soft interrupt request{reset}", bytes2human(cpu_times.softirq)],
-    [f"{green}steal{reset}", bytes2human(cpu_times.steal)],
-    [f"{green}guest{reset}", bytes2human(cpu_times.guest)],
-    [f"{green}guest nice{reset}", bytes2human(cpu_times.guest_nice)]
+    [f"{green}user{reset}", f"{cyan}{bytes2human(cpu_times.user)}{reset}"],
+    [f"{green}nice{reset}", f"{cyan}{bytes2human(cpu_times.nice)}{reset}"],
+    [f"{green}system{reset}", f"{cyan}{bytes2human(cpu_times.system)}{reset}"],
+    [f"{green}idle{reset}", f"{cyan}{bytes2human(cpu_times.idle)}{reset}"],
+    [f"{green}iowait{reset}", f"{cyan}{bytes2human(cpu_times.iowait)}{reset}"],
+    [f"{green}interrupt request{reset}", f"{cyan}{bytes2human(cpu_times.irq)}{reset}"],
+    [f"{green}soft interrupt request{reset}", f"{cyan}{bytes2human(cpu_times.softirq)}{reset}"],
+    [f"{green}steal{reset}", f"{cyan}{bytes2human(cpu_times.steal)}{reset}"],
+    [f"{green}guest{reset}", f"{cyan}{bytes2human(cpu_times.guest)}{reset}"],
+    [f"{green}guest nice{reset}", f"{cyan}{bytes2human(cpu_times.guest_nice)}{reset}"]
 ]
 
 cpu_statistics = [
-    [f"{green}ctx switches{reset}", cpu_stats.ctx_switches],
-    [f"{green}interrupts{reset}", cpu_stats.interrupts],
-    [f"{green}soft interrupts{reset}", cpu_stats.soft_interrupts],
-    [f"{green}syscalls{reset}", cpu_stats.syscalls]
+    [f"{green}ctx switches{reset}", f"{cyan}{cpu_stats.ctx_switches}{reset}"],
+    [f"{green}interrupts{reset}", f"{cyan}{cpu_stats.interrupts}{reset}"],
+    [f"{green}soft interrupts{reset}", f"{cyan}{cpu_stats.soft_interrupts}{reset}"],
+    [f"{green}syscalls{reset}", f"{cyan}{cpu_stats.syscalls}{reset}"]
 ]
 
 cpu_frequencies = [
-    [f"{green}current{reset}", cpu_freq.current],
-    [f"{green}min{reset}", cpu_freq.min],
-    [f"{green}max{reset}", cpu_freq.max]
+    [f"{green}current{reset}", f"{cyan}{cpu_freq.current}{reset}"],
+    [f"{green}min{reset}", f"{cyan}{cpu_freq.min}{reset}"],
+    [f"{green}max{reset}", f"{cyan}{cpu_freq.max}{reset}"]
 ]
 
 cpu_average = [
-    [f"{green}load average 1{reset}", load_average[0]],
-    [f"{green}load average 2{reset}", load_average[1]],
-    [f"{green}load average 3{reset}", load_average[2]]
+    [f"{green}load average 1{reset}", f"{cyan}{load_average[0]}{reset}"],
+    [f"{green}load average 2{reset}", f"{cyan}{load_average[1]}{reset}"],
+    [f"{green}load average 3{reset}", f"{cyan}{load_average[2]}{reset}"]
 ]
 
 # Memory
 
 vir_mem = [
-    [f"{green}total{reset}", bytes2human(virtual_memory.total)],
-    [f"{green}available{reset}", bytes2human(virtual_memory.available)],
-    [f"{green}percent{reset}", bytes2human(virtual_memory.percent)],
-    [f"{green}used{reset}", bytes2human(virtual_memory.used)],
-    [f"{green}free{reset}", bytes2human(virtual_memory.free)],
-    [f"{green}active{reset}", bytes2human(virtual_memory.active)],
-    [f"{green}inactive{reset}", bytes2human(virtual_memory.inactive)],
-    [f"{green}buffers{reset}", bytes2human(virtual_memory.buffers)],
-    [f"{green}cached{reset}", bytes2human(virtual_memory.cached)],
-    [f"{green}shared{reset}", bytes2human(virtual_memory.shared)],
-    [f"{green}slab{reset}", bytes2human(virtual_memory.slab)]
+    [f"{green}total{reset}", f"{cyan}{bytes2human(virtual_memory.total)}{reset}"],
+    [f"{green}available{reset}", f"{cyan}{bytes2human(virtual_memory.available)}{reset}"],
+    [f"{green}percent{reset}", f"{cyan}{bytes2human(virtual_memory.percent)}{reset}"],
+    [f"{green}used{reset}", f"{cyan}{bytes2human(virtual_memory.used)}{reset}"],
+    [f"{green}free{reset}", f"{cyan}{bytes2human(virtual_memory.free)}{reset}"],
+    [f"{green}active{reset}", f"{cyan}{bytes2human(virtual_memory.active)}{reset}"],
+    [f"{green}inactive{reset}", f"{cyan}{bytes2human(virtual_memory.inactive)}{reset}"],
+    [f"{green}buffers{reset}", f"{cyan}{bytes2human(virtual_memory.buffers)}{reset}"],
+    [f"{green}cached{reset}", f"{cyan}{bytes2human(virtual_memory.cached)}{reset}"],
+    [f"{green}shared{reset}", f"{cyan}{bytes2human(virtual_memory.shared)}{reset}"],
+    [f"{green}slab{reset}", f"{cyan}{bytes2human(virtual_memory.slab)}{reset}"]
 ]
 
 swap_mem = [
-    [f"{green}total{reset}", bytes2human(swap_memory.total)],
-    [f"{green}used{reset}", bytes2human(swap_memory.used)],
-    [f"{green}free{reset}", bytes2human(swap_memory.free)],
-    [f"{green}percent{reset}", f"{swap_memory.free:.2%}"],
-    [f"{green}swap-in{reset}", bytes2human(swap_memory.sin)],
-    [f"{green}swap-out{reset}", bytes2human(swap_memory.sout)]
+    [f"{green}total{reset}", f"{cyan}{bytes2human(swap_memory.total)}{reset}"],
+    [f"{green}used{reset}", f"{cyan}{bytes2human(swap_memory.used)}{reset}"],
+    [f"{green}free{reset}", f"{cyan}{bytes2human(swap_memory.free)}{reset}"],
+    [f"{green}percent{reset}", f"{cyan}{swap_memory.free:.2%}{reset}"],
+    [f"{green}swap-in{reset}", f"{cyan}{bytes2human(swap_memory.sin)}{reset}"],
+    [f"{green}swap-out{reset}", f"{cyan}{bytes2human(swap_memory.sout)}{reset}"]
 ]
 
 # Disk
 
 dsk_usg = [
-    [f"{green}total{reset}", bytes2human(disk_usage.total)],
-    [f"{green}used{reset}", bytes2human(disk_usage.used)],
-    [f"{green}free{reset}", bytes2human(disk_usage.free)],
-    [f"{green}percent{reset}", f"{disk_usage.percent:.2%}"]
+    [f"{green}total{reset}", f"{cyan}{bytes2human(disk_usage.total)}{reset}"],
+    [f"{green}used{reset}", f"{cyan}{bytes2human(disk_usage.used)}{reset}"],
+    [f"{green}free{reset}", f"{cyan}{bytes2human(disk_usage.free)}{reset}"],
+    [f"{green}percent{reset}", f"{cyan}{disk_usage.percent:.2%}{reset}"]
 ]
